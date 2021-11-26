c1628737e2d567b6aa95efcf0f66ed008adab183
Mogakamo <mogaka.amo254@gmail.com>
2021-11-27 01:01:44 +0300

    not yet configured maps but will do it tomorrow

--- app/src/main/java/com/mogaka/dev/healthy/MainActivity.kt
+++ app/src/main/java/com/mogaka/dev/healthy/MainActivity.kt
@@ -2,10 +2,13 @@ package com.mogaka.dev.healthy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment: SupportMapFragment = findViewById(R.id.maps)
     }
 }
